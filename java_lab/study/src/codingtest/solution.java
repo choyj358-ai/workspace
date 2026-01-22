12be63fb5646549e969c5ae4d6f1b18376d91b62
yujicoding <choyj358@gmail.com>
2026-01-22 17:56:17 +0900

    2026-01-22

---
+++ java_lab/study/src/codingtest/solution.java
@@ -0,0 +1,31 @@
+package codingtest;
+
+public class solution {
+
+    public static int[] solution(String[] strlist) {
+
+        // 1. 문자열 개수만큼의 int 배열 생성
+        int[] answer = new int[strlist.length];
+
+        // 2. 문자열 배열을 처음부터 끝까지 반복
+
+
+        // 3. 현재 문자열의 길이를 구해서
+
+
+        // 4. 같은 인덱스의 answer 배열에 저장
+
+
+
+
+        return answer;
+    }
+
+    static void main() {
+        String[] strlist = {"We", "are", "the", "world!"};
+
+        System.out.println(solution(strlist));
+        //[2, 3, 3, 6]
+    }
+
+}
